1a317c726b901e2e1a7227c15d528d53c2793e8f
Le Trong Thang <thangtl@hitachiconsulting.com>
2020-03-16 17:35:05 +0700

    finish styling menu items

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,39 +6,8 @@ const HomePage = () => {
   return (
     <div className='homepage'>
     <h1>Welcome to my Homepage</h1>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>HATS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>JACKETS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>SHOES</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>WOMENS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>MENS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
+      <Directory/>
     </div>
-  </div>
   );
 };
 
