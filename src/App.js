0f6006ec6ec8b67f57df7a34ec3fe8f19232a513
Le Trong Thang <thangtl@hitachiconsulting.com>
2020-03-17 11:29:47 +0700

    Add homepage and item colleciton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
@@ -17,9 +12,10 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
-}
+} 
+ 
 export default App;
